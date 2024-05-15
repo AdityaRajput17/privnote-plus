4c7412b3c49892e2ae7d80c5c0c12b9eb66443a6
Aditya Rajput <adityarajputwork@gmail.com>
2024-05-16 00:21:26 +0530

    hash password before saving in database

--- backend/controllers/createNote.js
+++ backend/controllers/createNote.js
@@ -1,35 +1,50 @@
 import {User} from "../models/user.model.js"
 import {Note} from "../models//note.model.js"
 import ShortUniqueId from "short-unique-id"
+import { hashPassword } from "../helpers/auth.js";
+import { encrypt, decrypt } from "../helpers/encrypt.js";
 
 export const createNote= async (req,res)=>{
     const {noteData, email} = req.body;
     const uid= new ShortUniqueId();
+    const id=uid.rnd();
+
+    const hashedPassword="";
+    if(noteData.optionData.password){
+        hashedPassword=await hashPassword(noteData.optionData.password);
+    }
+    
+    const note=new Note(
+    {
+        id:id,
+        note:noteData.note,
+        password:hashedPassword,
+        expiry:noteData.optionData.expiry,
+        DontWarn:noteData.optionData.DontWarn,
+
+    })
+    note.save()
 
     if(email)
     {
-        try {
-            const id=uid.rnd();
-            
-            const note=new Note(
-            {
-                id:id,
-                note:noteData.note,
-                password:noteData.optionData.password,
-                expiry:noteData.optionData.expiry,
-                DontWarn:noteData.optionData.DontWarn,
+        try {     
 
-            })
-            note.save()
-            
-            await User.updateOne({email:email},{$push: { notes: note } })
-            
+            await  User.updateOne({email:email},{$push: { notes: note } })
+            res.status(200).send("Note created!");
+
         } catch (error) {
-            throw error;
-            console.log("Error while creating a note");
+            console.log("Error while creating a note",error)
         }
     }
     else{
-        //TODO: create note with no user
+        //create note when not login-ed
+        try {
+        
+            await  User.updateOne({email:"ADMIN"},{$push: { notes: note } })
+            res.status(200).send("Note created!");
+
+        } catch (error) {
+            console.log("Error while creating a note",error)
+        }
     }
 }
