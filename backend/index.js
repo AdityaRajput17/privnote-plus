09c757017ea6d105ed3f3b769bc02a7a158528b7
Aditya Rajput <adityarajputwork@gmail.com>
2025-05-01 20:00:43 +0530

    Update CORS configuration

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app=express()
 
 // CORS configuration
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: ['https://privnote-plus.vercel.app', 'http://localhost:5173'],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
